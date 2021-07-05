11466f59f53b8f0db3c769e7c884e8180777ebe4
Karamysheva Irina <karamyshevaiv@mail.ru>
2021-07-05 15:09:07 +0500

    Finish "coats" app

--- app/src/main/java/ru/geekbrains/lesson6/CitiesFragment.java
+++ app/src/main/java/ru/geekbrains/lesson6/CitiesFragment.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.lesson6;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +12,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class CitiesFragment extends Fragment {
 
     // При создании фрагмента укажем его макет
+    private boolean isLandscape;
+    public static final String CURRENT_CITY = "CurrentCity";
+    private int currentPosition = 0;    // Текущая позиция (выбранный город)
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,29 +37,74 @@ public class CitiesFragment extends Fragment {
 
     // создаём список городов на экране из массива в ресурсах
     private void initList(View view) {
-        LinearLayout layoutView = (LinearLayout)view;
+        LinearLayout layoutView = (LinearLayout) view;
         String[] cities = getResources().getStringArray(R.array.cities);
 
         // В этом цикле создаём элемент TextView,
         // заполняем его значениями,
         // и добавляем на экран.
         // Кроме того, создаём обработку касания на элемент
-        for(int i=0; i < cities.length; i++){
+        for (int i = 0; i < cities.length; i++) {
             String city = cities[i];
             TextView tv = new TextView(getContext());
             int finalI = i;
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(getActivity(),CoatOfArmsActivity.class);
-                    intent.putExtra(CoatOfArmsFragment.ARG_INDEX, finalI);
-                    startActivity(intent);
+                    currentPosition = finalI;
+                    showCoatOfArms(currentPosition);
                 }
             });
             tv.setText(city);
             tv.setTextSize(30);
             layoutView.addView(tv);
-
         }
+    }
+    // Сохраним текущую позицию (вызывается перед выходом из фрагмента)
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(CURRENT_CITY, currentPosition);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void showPortCoatOfArms(int finalI) {
+        Intent intent = new Intent(getActivity(), CoatOfArmsActivity.class);
+        intent.putExtra(CoatOfArmsFragment.ARG_INDEX, finalI);
+        startActivity(intent);
+    }
+    // activity создана, можно к ней обращаться. Выполним начальные действия
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Определение, можно ли будет расположить рядом герб в другом фрагменте
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+
+        // Если можно нарисовать рядом герб, то сделаем это
+        if (isLandscape) {
+            showLandCoatOfArms(0);
+        }
+    }
+
+    private void showCoatOfArms(int index) {
+        if (isLandscape) {
+            showLandCoatOfArms(index);
+        } else {
+            showPortCoatOfArms(index);
+        }
+    }
+
+    // Показать герб в ландшафтной ориентации
+    private void showLandCoatOfArms(int index) {
+        // Создаём новый фрагмент с текущей позицией для вывода герба
+        CoatOfArmsFragment detail = CoatOfArmsFragment.newInstance(index);
+
+        // Выполняем транзакцию по замене фрагмента
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.coat_of_arms, detail);  // замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
     }
 }
